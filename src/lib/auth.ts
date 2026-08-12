298be0dd98909335aea87cdf0ff809a1b4abcadf
nieukki <tomekpacek@o2.pl>
2026-08-12 12:28:31 +0200

    feat: better-auth and drizzle-orm config:

---
+++ src/lib/auth.ts
@@ -0,0 +1,19 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db/index";
+import { nextCookies } from "better-auth/next-js";
+import { lastLoginMethod } from "better-auth/plugins";
+import * as schema from "@/db/schema";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: schema,
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
+
+  plugins: [nextCookies(), lastLoginMethod()],
+});
