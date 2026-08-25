0f98cd191a759126bc22a77bef1df9791aa481be
JakubBizon <kubabizon97@gmail.com>
2026-08-25 11:49:23 +0200

    chore: remove unused imports

--- src/app/_components/home/logos-section.tsx
+++ src/app/_components/home/logos-section.tsx
@@ -1,7 +1,6 @@
 "use client";
 import LogoLoop from "@/components/LogoLoop";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+
 import {
   SiApple,
   SiDell,
