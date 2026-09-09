fafe31f59546a6edf76f29c6d3f02b9f7a20e4a9
JakubBizon <kubabizon97@gmail.com>
2026-09-09 13:21:03 +0200

    feat: implement initial add to cart functionality in product card

--- src/app/products/_components/product-card.tsx
+++ src/app/products/_components/product-card.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,12 +7,31 @@ import ActionButton from "@/components/shared/action-button";
 import { ShoppingCartIcon } from "lucide-react";
 import { Laptop } from "@/db/schema";
 import ProductCardSpecs from "./product-card-specs";
+import { useTransition } from "react";
+import { addToCart } from "@/actions/cart";
+import { toast } from "@/components/ui/toast";
 
 type Props = {
   laptop: Laptop;
 };
 
 export default function ProductCard({ laptop }: Props) {
+  const [isPending, startTransition] = useTransition();
+
+  const handleAddToCart = () => {
+    startTransition(async () => {
+      try {
+        await addToCart(laptop.id, 1);
+        toast.add({ title: "Product added to cart", type: "success" });
+      } catch (error) {
+        toast.add({
+          title: `Something went wrong: ${error}`,
+          type: "error",
+        });
+      }
+    });
+  };
+
   return (
     <Card className="h-full p-0">
       <CardContent className="h-full p-1.5">
@@ -35,7 +55,12 @@ export default function ProductCard({ laptop }: Props) {
             <p className="font-semibold md:text-lg">
               {formatPrice(laptop.priceCents)}
             </p>
-            <ActionButton className="shrink-0 rounded-full p-2">
+
+            <ActionButton
+              onClick={handleAddToCart}
+              isPending={isPending}
+              className="shrink-0 rounded-full p-2"
+            >
               <ShoppingCartIcon size={20} />
             </ActionButton>
           </div>
