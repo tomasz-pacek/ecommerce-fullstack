4a1c56ca42f2d29ce5d7407f54b25dd1345d668f
JakubBizon <kubabizon97@gmail.com>
2026-09-14 19:50:52 +0200

    feat: add empty state message in products grid

--- src/app/products/_components/product-grid.tsx
+++ src/app/products/_components/product-grid.tsx
@@ -7,10 +7,21 @@ type Props = {
 
 export default function ProductGrid({ products }: Props) {
   return (
-    <div className="mx-auto grid grid-cols-1 content-start items-start gap-4 sm:grid-cols-2 md:grid-cols-3">
-      {products.map((product) => (
-        <ProductCard key={product.id} laptop={product} />
-      ))}
-    </div>
+    <>
+      {products.length === 0 && (
+        <div className="flex flex-col items-center justify-center gap-2 py-2">
+          <p className="text-2xl">No products found</p>
+          <p className="text-muted-foreground">
+            Try removing selected filters to expand your search{" "}
+          </p>
+        </div>
+      )}
+
+      <div className="mx-auto grid grid-cols-1 content-start items-start gap-4 sm:grid-cols-2 md:grid-cols-3">
+        {products.map((product) => (
+          <ProductCard key={product.id} laptop={product} />
+        ))}
+      </div>
+    </>
   );
 }
